10a39b2c2f3233ac6b234bd31cb91ea902b88549
Rong Yu <rongyu9124@hotmail.com>
2021-02-08 20:59:04 +0800

    update sdf for hands

--- sdf/__init__.py
+++ sdf/__init__.py
@@ -1,2 +1,3 @@
 from .sdf import sdf, SDF
-from .sdf_loss import SDFLoss
+from .sdf_loss_hand import SDFLoss
+from .sdf_loss_origin import SDFLoss_
--- sdf/sdf_loss_origin.py
+++ sdf/sdf_loss_origin.py
@@ -5,10 +5,10 @@ import sys
 from sdf import SDF
 import pdb
 
-class SDFLoss(nn.Module):
+class SDFLoss_(nn.Module):
 
     def __init__(self, faces, grid_size=32, robustifier=None, debugging=False):
-        super(SDFLoss, self).__init__()
+        super(SDFLoss_, self).__init__()
         self.sdf = SDF()
         self.register_buffer('faces', torch.tensor(faces.astype(np.int32)))
         self.grid_size = grid_size
@@ -27,14 +27,8 @@ class SDFLoss(nn.Module):
     @torch.no_grad()
     def check_overlap(self, bbox1, bbox2):
         # check x
-        # print('bbox1', bbox1[:, 0])
-        # print('bbox2', bbox2[:, 0])
-        # pdb.set_trace()
-        #print(bbox1[0,0] > bbox2[1,0] or bbox2[0,0] > bbox1[1,0])
         if bbox1[0,0] > bbox2[1,0] or bbox2[0,0] > bbox1[1,0]:
             return False
-        #else:
-            #print('out if')
         #check y
         if bbox1[0,1] > bbox2[1,1] or bbox2[0,1] > bbox1[1,1]:
             return False
@@ -56,27 +50,13 @@ class SDFLoss(nn.Module):
             isolated[i] = isolated_i
         return isolated
 
-    def forward(self, vertices, translation, scale_factor=0.2):
-        num_people = vertices.shape[0]
-        # If only one person in the scene, return 0
-        # vertices = vertices + translation.unsqueeze(dim=1)
-        loss = torch.tensor(0., device=vertices.device)
-        if num_people == 1:
-            return loss
+
+    def forward(self, vertices, scale_factor=0.2):
+        num_hand = vertices.shape[0]
         boxes = self.get_bounding_boxes(vertices)
-        isolated_boxes = self.filter_isolated_boxes(boxes)
-        isolated_boxes = isolated_boxes.type(torch.bool)
-        overlapping_boxes = ~isolated_boxes
-        print(overlapping_boxes)
+        loss = torch.tensor(0., device=vertices.device)
 
-        # If no overlapping voxels return 0
-        if overlapping_boxes.sum() == 0:
-            return loss
-
-        # Filter out the isolated boxes
-        vertices = vertices[overlapping_boxes, :].contiguous()
-        # translation = translation[overlapping_boxes, :].contiguous()
-        boxes = boxes[overlapping_boxes]
+        # re-scale the input vertices
         boxes_center = boxes.mean(dim=1).unsqueeze(dim=1)
         boxes_scale = (1+scale_factor) * 0.5*(boxes[:,1] - boxes[:,0]).max(dim=-1)[0][:,None,None]
         with torch.no_grad():
@@ -87,24 +67,27 @@ class SDFLoss(nn.Module):
             phi = self.sdf(self.faces, vertices_centered_scaled)
             assert(phi.min() >= 0)
 
-        valid_people = vertices.shape[0]
-        # Convert vertices to the format expected by grid_sample
-        for i in range(valid_people):
-            weights = torch.ones(valid_people, 1, device=vertices.device)
+        pdb.set_trace()
+        
+        #for i in range(num_hand):
+        for i in [1,]:
+            weights = torch.ones(num_hand, 1, device=vertices.device)
             weights[i,0] = 0.
             # Change coordinate system to local coordinate system of each person
             vertices_local = (vertices - boxes_center[i].unsqueeze(dim=0)) / boxes_scale[i].unsqueeze(dim=0)
             vertices_grid = vertices_local.view(1,-1,1,1,3)
             # Sample from the phi grid
-            phi_val = nn.functional.grid_sample(phi[i][None, None], vertices_grid, align_corners=True).view(valid_people, -1)
+            phi_val = nn.functional.grid_sample(
+                phi[i][None, None], vertices_grid, align_corners=True).view(num_hand, -1)
+            # print(phi.size(), vertices_local.size(), vertices_grid.size(), phi_val.size())
+            # sys.exit(0)
             # ignore the phi values for the i-th shape
             cur_loss = weights * phi_val
-            if self.debugging:
-                import ipdb;ipdb.set_trace()
             # robustifier
+            # cur_loss = cur_loss^2 / (cur_loss^2 + robust^2)
             if self.robustifier:
                 frac = (cur_loss / self.robustifier) ** 2
                 cur_loss = frac / (frac + 1)
 
-            loss += cur_loss.sum() / valid_people ** 2
+            loss += cur_loss.sum() / num_hand ** 2
         return loss
